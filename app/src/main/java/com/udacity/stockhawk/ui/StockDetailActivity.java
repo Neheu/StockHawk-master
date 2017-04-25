c8e7d485381200a980023a777a0b468be7c50dfe
Neha <neha.sharma.unique@gmail.com>
2017-04-25 13:23:26 +0530

    code to fetch stock

--- app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
+++ app/src/main/java/com/udacity/stockhawk/ui/StockDetailActivity.java
@@ -22,25 +22,42 @@ import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.udacity.stockhawk.R;
 import com.udacity.stockhawk.data.Contract;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.StringTokenizer;
 
 
 public class StockDetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private String symbol;
-
+    private ArrayList<BarEntry> _stockList=new ArrayList<>();
+    private ArrayList<String>_dateDataList = new ArrayList<>();
+    private LineChart _stockChart;
+    List<Entry> entries = new ArrayList<>();
+    List<Float> timeData = new ArrayList<>();
+    List<Float> stockPrice = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stock_detail);
-        LineChart chart = (LineChart) findViewById(R.id.chart);
+        _stockChart = (LineChart) findViewById(R.id.chart);
 
-        /*BarData data = new BarData(getXAxisValues(), getDataSet());
-        chart.setData(data);
-//        chart.setDescription("My Chart");
-        chart.animateXY(2000, 2000);
-        chart.invalidate();*/
+
         symbol = getIntent().getStringExtra("symbol");
         getSupportLoaderManager().initLoader(1, null, this);
 
@@ -105,26 +122,72 @@ public class StockDetailActivity extends AppCompatActivity implements LoaderMana
 
     }
 
+
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-        //String label = data.getString(data.getColumnIndexOrThrow(Contract.Quote.COLUMN_PRICE));
+      //  SimpleDateFormat format = new SimpleDateFormat("dd:mm:yyyy");
         if (cursor != null && cursor.moveToFirst())
-
             do {
 
-                String a = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Quote.COLUMN_PRICE));
+                String history = cursor.getString(cursor.getColumnIndexOrThrow(Contract.Quote.COLUMN_HISTORY));
+                String[] dataPairs = history.split("\n");
 
-                Toast.makeText(this, a + " nnnn", Toast.LENGTH_SHORT).show();
+
+                for(String data: dataPairs)
+                {
+                    String splitData[] = data.split(",");
+                    timeData.add(Float.valueOf(splitData[0]));
+                    stockPrice.add(Float.valueOf(splitData[1]));
+//                    try {
+//                        Date date = format.parse(splitData[0]);
+//                        Calendar calendar = Calendar.getInstance();
+//                        calendar.setTime(date);
+//
+//                        String stringDate = String.valueOf(calendar.get(Calendar.MONTH)
+//                                +"/"+ String.valueOf(calendar.get(Calendar.DAY_OF_MONTH))
+//                                +"/"+String.valueOf(calendar.get(Calendar.YEAR)));
+//
+//                        _dateDataList.add(stringDate);
+//                    } catch (ParseException e) {
+//                        e.printStackTrace();
+//                    }
+                    Entry entry = new Entry(Float.valueOf(splitData[1]),Float.valueOf(splitData[0]));
+                    entries.add(entry);
+                }
+
 
 
             }
             while (cursor.moveToNext());
+       // LineDataSet dataSet = new LineDataSet(entries,"Lable...");
+       // _stockChart.sets
+        ArrayList<ILineDataSet> sets = (ArrayList<ILineDataSet>) _stockChart.getData()
+                .getDataSets();
+        for (ILineDataSet set : sets) {
+            set.setDrawFilled(true);
+        }
+
+       // LineData data = new LineData(sets);
+       // _stockChart.setData(data);
+        _stockChart.animateXY(2000, 2000);
+       // LineData data = new LineData(getXAxisValues(),getDataSet());
+
+       // setUpChart(_stockChart, data);
+
+        _stockChart.invalidate();
+
     }
+
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
     }
+
+
+
+
+
 
 
 }
